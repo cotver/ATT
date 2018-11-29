c1f10e40ceedc2e2757234698e5655446f4bdbef
Tanachot Chirakitsakul <42908486+cotver@users.noreply.github.com>
2018-11-29 20:38:16 +0700

    Update top10.py

--- modules/top10.py
+++ modules/top10.py
@@ -3,11 +3,12 @@
 def year_country(data, months, continents):
     """find top 10 country in that year"""
     dic = {}
-    for month in months :
-        for continent in continents:
-            for country in data[month][continent]:
-                dic[country] = data[month][continent][country]+dic[country] \
-                if country in dic else data[month][continent][country]
+    for month in months:
+        if month in data:
+            for continent in continents:
+                for country in data[month][continent]:
+                    dic[country] = data[month][continent][country]+dic[country] \
+                    if country in dic else data[month][continent][country]
     topten = sorted(dic, key=lambda x: dic[x], reverse =True)
     return sortdata(topten, dic)
     
@@ -15,11 +16,12 @@ def year_country(data, months, continents):
 def year_continent(data, months, continents):
     """find top 10 continent in that year"""
     dic = {}
-    for month in months :
-        for continent in continents:
-            for country in data[month][continent]:
-                dic[continent] = data[month][continent][country]+dic[continent] \
-                if continent in dic else data[month][continent][country]
+    for month in months:
+        if month in data:
+            for continent in continents:
+                for country in data[month][continent]:
+                    dic[continent] = data[month][continent][country]+dic[continent] \
+                    if continent in dic else data[month][continent][country]
     topten = sorted(dic, key=lambda x: dic[x], reverse =True)
     return sortdata(topten, dic)
 
@@ -27,11 +29,12 @@ def total_country(data, years, months, continents):
     """find top 10 country 2016-2018"""
     dic = {}
     for year in years:
-        for month in months if year != "2018" else months[0:10]:
-            for continent in continents:
-                for country in data[year][month][continent]:
-                    dic[country] = data[year][month][continent][country]+dic[country] \
-                    if country in dic else data[year][month][continent][country]
+        for month in months:
+            if month in data[year]:
+                for continent in continents:
+                    for country in data[year][month][continent]:
+                        dic[country] = data[year][month][continent][country]+dic[country] \
+                        if country in dic else data[year][month][continent][country]
     topten = sorted(dic, key=lambda x: dic[x], reverse =True)
     return sortdata(topten, dic)
 
@@ -40,11 +43,12 @@ def total_continent(data, years, months, continents):
     """find top 10 continent 2016-2018"""
     dic = {}
     for year in years:
-        for month in months if year != "2018" else months[0:10]:
-            for continent in continents:
-                for country in data[year][month][continent]:
-                    dic[continent] = data[year][month][continent][country]+dic[continent] \
-                    if continent in dic else data[year][month][continent][country]
+        for month in months:
+            if month in data[year]:
+                for continent in continents:
+                    for country in data[year][month][continent]:
+                        dic[continent] = data[year][month][continent][country]+dic[continent] \
+                        if continent in dic else data[year][month][continent][country]
     topten = sorted(dic, key=lambda x: dic[x], reverse =True)
     return sortdata(topten, dic)
         
@@ -55,4 +59,3 @@ def sortdata(topten, dic):
         lis.append((sor_t, dic[sor_t]))
         
     return lis
-
