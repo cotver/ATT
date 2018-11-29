7642c7fc39bfe835a2445b5d92bdbec6e6c3aa6c
Tanachot Chirakitsakul <42908486+cotver@users.noreply.github.com>
2018-11-29 20:39:01 +0700

    Add files via upload

---
+++ modules/season.py
@@ -0,0 +1,31 @@
+"""
+People come in each season
+"""
+
+def season(data, years, months):
+    
+    dic = {}
+    summer = ["Mar", "Apr", "May"]
+    rainy = ["Jun", "Jul", "Aug", "Sep", "Oct"]
+    winter = ["Jan", "Feb", "Nov", "Dec"]
+
+    
+    for year in years:
+        count_summer = 0
+        count_rainy = 0
+        count_winter = 0
+        for month in data[year]:
+            count = 0
+            for continent in data[year][month]:
+                for country in data[year][month][continent]:
+                    count += data[year][month][continent][country]
+            if month in summer:
+                count_summer += count
+            elif month in rainy:
+                count_rainy += count
+            elif month in winter:
+                count_winter += count
+                
+        dic[year] = [count_summer, count_rainy, count_winter]
+
+    return dic
